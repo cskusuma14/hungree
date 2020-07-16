9d1a22c07fa2bd0952519df4585d6444f1578712
cskusuma14 <chandrasuryakusuma14@gmail.com>
2020-07-16 23:29:46 +0700

    setup database

---
+++ server/models/product.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Product name cannot be empty",
+          },
+        },
+      },
+      price: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Price name cannot be empty",
+          },
+        },
+      },
+      stock: DataTypes.INTEGER,
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Category cannot be empty",
+          },
+        },
+      },
+      description: DataTypes.STRING,
+      image_url: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
+  return Product;
+};
